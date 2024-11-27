e2fb708ebd8420ce5d4499cd5ee70fd3a353e43e
Dante-Lucas <lucassilva270805@gmail.com>
2024-11-27 13:13:52 -0300

    Atualizando o arquivo curso de schema.

--- src/schema/curso.schema.ts
+++ src/schema/curso.schema.ts
@@ -2,14 +2,21 @@ import { curso } from '@/db/schema/cursos.ts'
 import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
 import { z } from 'zod'
 
+const categoriaSchema = z.object({
+  categoriaId: z.number(),
+  name: z.string(),
+})
+const professorSchema = z.object({
+  professorId: z.number(),
+  name: z.string(),
+})
+
 export const cursoSchema = z.object({
   cursoId: z.number(),
   titulo: z.string(),
   valor: z.string(),
-  categoriaId: z.number(),
-  categoriaName: z.string(),
-  professorId: z.number(),
-  professorName: z.string(),
+  categoria: categoriaSchema,
+  professor: professorSchema,
 })
 
 export const cursoInsertSchema = createInsertSchema(curso)
