116d9bc74e362c91d91d3f55975acf846214adff
Dante-Lucas <lucassilva270805@gmail.com>
2024-11-27 13:19:15 -0300

    Atualização geral

--- src/utils/curso-response.ts
+++ src/utils/curso-response.ts
@@ -1,19 +1,19 @@
 import type { Curso, Cursos } from '@/@types/cursos.ts'
 
 export function curseResponse(curse: Cursos[]): Curso[] {
-    return curse.map(curse => {
-        return {
-            cursoId: curse.cursoId,
-            titulo: curse.titulo,
-            valor: curse.valor,
-            categoria: {
-              categoriaId: curse.categoriaId,
-              name: curse.categoriaName,
-            },
-            professor: {
-              professorId: curse.professorId,
-              name: curse.professorName,
-            },
-          }
-    })
+  return curse.map(curse => {
+    return {
+      cursoId: curse.cursoId,
+      titulo: curse.titulo,
+      valor: curse.valor,
+      categoria: {
+        categoriaId: curse.categoriaId,
+        name: curse.categoriaName,
+      },
+      professor: {
+        professorId: curse.professorId,
+        name: curse.professorName,
+      },
+    }
+  })
 }
