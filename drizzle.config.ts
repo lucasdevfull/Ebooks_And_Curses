98ccf0d41c89aa4963610dcc809ed2cbb8302153
lucasdevfull <lucassilva270805@gmail.com>
2025-03-23 20:40:52 -0300

    Alterando as configurações do projeto

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,12 @@
 
-import { env } from '@/infrastructure/env.ts'
+import { DATABASE_URL } from '@/infrastructure/env.ts'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
   schema: './src/db/schema/*',
   out: '.drizzle/migrations',
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: DATABASE_URL,
   },
   dialect: "postgresql",
 })
