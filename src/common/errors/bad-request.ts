cf394dde664ef78b66a19ae547b94f2818dd8f55
lucasdevfull <lucassilva270805@gmail.com>
2025-04-10 18:06:31 -0300

    Adicionando o enum na classe de erro.

--- src/common/errors/bad-request.ts
+++ src/common/errors/bad-request.ts
@@ -3,7 +3,7 @@ import { HttpStatus } from '../enum/http.ts'
 
 export class BadRequestError extends HttpException {
   constructor(message: string) {
-    super(message, HttpStatus.NOT_FOUND)
+    super(message, HttpStatus.BAD_REQUEST)
     this.name = 'BadRequestError'
   }
 }
