77a67d529b6789dd2c54790c2cb37c8f3f5c4b6f
lucasdevfull <lucassilva270805@gmail.com>
2025-04-11 12:00:53 -0300

    Atualizando as importações do arquivo main

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import { FastifyServer } from '@/infrastructure/server.ts'
-import { routes } from '@/infrastructure/router.ts'
+import { FastifyServer } from '@/infra/server.ts'
+import { routes } from '@/infra/router.ts'
 
 function bootstrap() {
   const server = new FastifyServer(routes)
