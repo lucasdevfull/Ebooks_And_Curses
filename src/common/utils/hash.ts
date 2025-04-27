8e6918a707fb59ba6e1c07f63370b1173b7b60ad
lucasdevfull <lucassilva270805@gmail.com>
2025-04-26 22:28:00 -0300

    Atualizando a função hash

--- src/common/utils/hash.ts
+++ src/common/utils/hash.ts
@@ -1,6 +1,6 @@
 import { hash } from "@node-rs/bcrypt"
 
 export async function passwordHash(password: string, salt: number) {
-  const pass = await hash(password, salt)
+  const pass = await hash(password,salt)
   return pass
 }
