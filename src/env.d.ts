0c2a8f00477314bb0968d767fe65623c98fe099a
lucasdevfull <lucassilva270805@gmail.com>
2025-08-04 17:14:36 -0300

    Atualização geral do projeto

---
+++ src/env.d.ts
@@ -0,0 +1,14 @@
+declare namespace NodeJS {
+    export interface ProcessEnv {
+        NODE_ENV: 'development' | 'production' | 'test'
+        DATABASE_URL?: string
+        SERVER_URL?: string
+        PORT?: string | number
+        JWT_SECRET?: string
+        EXPIRES_IN?: string
+        REFRESH_EXPIRES_IN?: string
+        EMAIL: string
+        GOOGLE_CLIENT_ID: string
+        GOOGLE_CLIENT_KEY: string
+    }
+}
